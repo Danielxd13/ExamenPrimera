c5353095cffecef577a54a95f6774b4fce67e887
Danielxd13 <dsuarezsoto2000@gmail.com>
2023-03-03 17:08:38 +0100

    base matematica

---
+++ matematicas/src/test/java/es/rlopezga/mates/matematicas/CalculadoraTest.java
@@ -0,0 +1,39 @@
+package es.rlopezga.mates.matematicas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testCalculadora() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSolucion1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSolucion2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEsPrimo() {
+		fail("Not yet implemented");
+	}
+
+}
