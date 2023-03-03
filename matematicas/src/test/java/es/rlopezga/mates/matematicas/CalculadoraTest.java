2c756059aac9492ecf96a43e166c44c4c7d35d92
Danielxd13 <dsuarezsoto2000@gmail.com>
2023-03-03 17:39:30 +0100

    primer test

--- matematicas/src/test/java/es/rlopezga/mates/matematicas/CalculadoraTest.java
+++ matematicas/src/test/java/es/rlopezga/mates/matematicas/CalculadoraTest.java
@@ -3,37 +3,37 @@ package es.rlopezga.mates.matematicas;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CalculadoraTest {
-
+	private Calculadora calculadora;
+	private Calculadora calculadora1;
+	private Calculadora calculadora2;
+	private Calculadora calculadora3;
+	
 	@BeforeEach
 	void setUp() throws Exception {
+		 calculadora1=new Calculadora();
+		 calculadora2=new Calculadora();
+		 calculadora3=new Calculadora();
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		calculadora1=null;
+		calculadora2=null;
+		calculadora3=null;
 	}
 
 	@Test
-	void testCalculadora() {
-		fail("Not yet implemented");
+	void testSolucion1Correcta() {
+		Assertions.assertEquals(calculadora1.solucion1(1, -5, 6), 3);
+		Assertions.assertEquals(calculadora2.solucion1(2, -7, 3), 3);
+		Assertions.assertEquals(calculadora3.solucion1(-1, -7, -10), -5);
 	}
 
-	@Test
-	void testSolucion1() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testSolucion2() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testEsPrimo() {
-		fail("Not yet implemented");
-	}
+	
 
 }
